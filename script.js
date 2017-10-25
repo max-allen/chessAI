72329ddc4b58f694742eb4cfbb86ca7eca47326a
Max Allen <maxallen@Maxs-MBP.home>
2017-10-24 20:40:16 -0400

    evaluation_option

--- script.js
+++ script.js
@@ -7,10 +7,58 @@ game = new Chess();
  config and functionality
  */
 
+//piece values given 13359 system
+const pieceValues = {
+	'p': 1,
+	'n':3,
+	'b':3,
+	'r':5,
+	'q':9,
+	'k':0
+}
+
 const getBestMove = () => {
 	let moves = game.ugly_moves();
-	return moves[Math.floor(Math.random() * moves.length)]
+
+	// return move with highest resulting board evaluation
+
+	let bestMove = null;
+	let maxValue = -9999 //dummy max for black
+
+	for (let i = 0; i < moves.length; i++){
+		let currentMove = moves[i]
+		game.ugly_move(currentMove);
+
+		let boardValue = -evaluateBoard(game.board())
+
+		if(boardValue > maxValue) {
+			bestMove = currentMove;
+		}
+
+		game.undo();
+	}
+
+	return bestMove
+
+
+	// return random move out of possible moves
+	// return moves[Math.floor(Math.random() * moves.length)]
 };
+
+const evaluateBoard = (board) => {
+	let evaluation = 0;
+	for(let i = 0; i < 8; i++){
+		for(let j = 0; j < 8; j++){
+			let piece = board[i][j];
+			if(piece) {
+				piece.color === 'w' ? evaluation += pieceValues[piece.type] : evaluation += -pieceValues[piece.type]
+			}
+		}
+	}
+	return evaluation;
+}
+
+
 
 const onDragStart = (source, piece, position, orientation) => {
 	  if (game.in_checkmate() === true || game.in_draw() === true ||
@@ -26,13 +74,14 @@ const makeBestMove = () => {
 	renderMoveHistory(game.history());
 }
 
+//temporarily disabling move history
 const renderMoveHistory = function (moves) {
-    let historyElement = $('#move-history').empty();
-    historyElement.empty();
-    for (let i = 0; i < moves.length; i += 2) {
-        historyElement.append('<span>' + moves[i] + ' ' + ( moves[i + 1] ? moves[i + 1] : ' ') + '</span><br>')
-    }
-    historyElement.scrollTop(historyElement[0].scrollHeight);
+    // let historyElement = $('#move-history').empty();
+    // historyElement.empty();
+    // for (let i = 0; i < moves.length; i += 2) {
+    //     historyElement.append('<span>' + moves[i] + ' ' + ( moves[i + 1] ? moves[i + 1] : ' ') + '</span><br>')
+    // }
+    // historyElement.scrollTop(historyElement[0].scrollHeight);
 }
 
 const onDrop = (source, target) => {
